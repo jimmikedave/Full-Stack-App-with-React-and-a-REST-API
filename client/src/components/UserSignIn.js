9607d661ef35d3ca845380d48d24bd84ff20e76e
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-08-22 07:43:54 -0400

    Configure UserSignIn

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -4,14 +4,14 @@ import Form from './Form';
 
 export default class UserSignIn extends Component {
   state = {
-    username: '',
+    emailAddress: '',
     password: '',
     errors: [],
   }
 
   render() {
     const {
-      username,
+      emailAddress,
       password,
       errors,
     } = this.state;
@@ -28,10 +28,10 @@ export default class UserSignIn extends Component {
             elements={() => (
               <React.Fragment>
                 <input 
-                  id="username" 
-                  name="username" 
+                  id="emailAddress" 
+                  name="emailAddress" 
                   type="text"
-                  value={username} 
+                  value={emailAddress} 
                   onChange={this.change} 
                   placeholder="User Name" />
                 <input 
@@ -63,10 +63,28 @@ export default class UserSignIn extends Component {
   }
 
   submit = () => {
+    // destructuring assignment to extract context prop from this.props
+    const { context } = this.props;
+    const { emailAddress, password } = this.state;
 
+    context.actions.signIn(emailAddress, password)
+      .then((user) => {
+        if(user === null) {
+          this.setState(() => {
+            return {errors: ['Sign-in was unsuccessful']};
+          });
+        } else {
+          this.props.history.push('/authenticated');
+          console.log(`SUCCESS! ${emailAddress} is now signed in!`)
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        this.props.history.push('/error');
+      })
   }
 
   cancel = () => {
-
+    this.props.history.push('/');
   }
 }
