9ade431fcfa9d50601302d62aeb724f726499835
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-08-22 07:44:03 -0400

    Configure Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default () => (
-    <div className="header">
-        <div className="bounds">
-            <h1 className="header--logo">Courses</h1>
-            <nav>
-                <React.Fragment>
-                    <Link className="signup" to="/signup">Sign Up</Link>
-                    <Link className="signin" to="/signin">Sign In</Link>
-                </React.Fragment>
-            </nav>
-        </div>
-    </div>
-);
+export default class Header extends React.PureComponent {
+    render () {
+        // extract context from this.props
+        const { context } = this.props;
+        const authUser = context.authenticatedUser;
+        console.log(authUser)
+
+
+        return (
+            <div className="header">
+                <div className="bounds">
+                    <h1 className="header--logo">Courses</h1>
+                    <nav>
+                        {
+                            authUser ? 
+                                <React.Fragment>
+                                    <span>Welcome, {authUser.authUser.firstName}!</span>
+                                    <Link to="/signout">Sign Out</Link>
+                                </React.Fragment>
+                            :
+                                <React.Fragment>
+                                    <Link className="signup" to="/signup">Sign Up</Link>
+                                    <Link className="signin" to="/signin">Sign In</Link>
+                                </React.Fragment>
+                        }
+                    </nav>
+                </div>
+            </div>
+        );
+    }
+};
