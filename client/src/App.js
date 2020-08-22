c3917e821fcb2055ba18c97e14e2a9ae4bd84fe2
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-08-22 07:44:37 -0400

    Add Context to Routes

--- client/src/App.js
+++ client/src/App.js
@@ -13,25 +13,34 @@ import UpdateCourse from './components/UpdateCourse';
 import UserSignIn from './components/UserSignIn';
 import UserSignOut from './components/UserSignOut';
 import UserSignUp from './components/UserSignUp';
+import Authenticated from './components/Authenticated';
 
-// New Import
+
+// Higher order components
 import withContext from './Context';
+import PrivateRoute from './PrivateRoute';
 
+// Connect Components to context
+const HeaderWithContext = withContext(Header);
+const AuthWithContext = withContext(Authenticated);
 const UserSignUpWithContext = withContext(UserSignUp);
+const UserSignInWithContext = withContext(UserSignIn);
+const UserSignOutWithContext = withContext(UserSignOut);
 
 
 
 export default () => (
   <Router>
     <div>
-      <Header />
+      <HeaderWithContext />
 
       <Switch>
         <Route exact path="/" component={Courses} />
+        <PrivateRoute path="/authenticated" component={AuthWithContext}/>
         <Route path="/courses/create" component={CreateCourse} />
-        <Route path="/signin" component={UserSignIn} />
+        <Route path="/signin" component={UserSignInWithContext} />
         <Route path="/signup" component={UserSignUpWithContext} />
-        <Route path="/signout" component={UserSignOut} />
+        <Route path="/signout" component={UserSignOutWithContext} />
       </Switch>
     </div>
   </Router>
