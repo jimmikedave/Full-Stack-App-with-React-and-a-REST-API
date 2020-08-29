10274f249972233fbc3d67734b43f5ef003d965a
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-08-29 19:44:53 -0400

    Update Data.js

--- client/src/Data.js
+++ client/src/Data.js
@@ -1,12 +1,12 @@
 import config from './config';
 
 export default class Data {
-  // used to make the GET and POST requests to the REST API
+  // Used to make the requests to the REST API.
   api(path, method = 'GET', body = null, requiresAuth = false, credentials = null) {
       const url = config.apiBaseUrl + path;
 
-      // configuration object that lets you control a number of different settings
-      // sends a request with the HTTP method along with request headers and a stringified body
+      // Configuration object that lets you control a number of different settings.
+      // Sends a request with the HTTP method along with request headers and a stringified body.
       const options = {
           method,
           headers: {
@@ -18,7 +18,7 @@ export default class Data {
           options.body = JSON.stringify(body);
       }
 
-      // Check if auth is required 
+      // Check if auth is required.
       if(requiresAuth) {
         const encodedCredentials = btoa(`${credentials.emailAddress}:${credentials.password}`);
 
@@ -28,7 +28,7 @@ export default class Data {
       return fetch(url, options);
   }
 
-  // makes a GET request to the /users endpoint, and returns a JSON object
+  // Makes a GET request to the /users endpoint, and returns a JSON object.
   async getUser(emailAddress, password) {
     const response = await this.api(`/users`, 'GET', null, true, { emailAddress, password });
     if (response.status === 200) {
@@ -42,7 +42,7 @@ export default class Data {
     }
   }
   
-  // makes a POST request, sending the new user data to the /users endpoint
+  // Makes a POST request, sending the new user data to the /users endpoint.
   async createUser(user) {
     const response = await this.api('/users', 'POST', user);
     if (response.status === 201) {
@@ -55,6 +55,66 @@ export default class Data {
     }
     else {
       throw new Error();
+    }
+  }
+
+  // Makes a GET request to courses.
+  async getCourses() {
+    const response = await this.api('/courses', 'GET')
+    if(response.status === 200) {
+      return response.json().then(data => data)
+    } else {
+      window.location.replace('/error')
+    }
+  }
+
+  // Makes a GET request to course information.
+  async getCourse(id) {
+    const response = await this.api('/courses/' + id, 'GET')
+    if(response.status === 200) {
+      return response.json().then(data => data)
+    } else {
+      window.location.replace('/error')
+    }
+  }
+
+  // Makes a DELETE request to delete a course.
+  async deleteCourse(id, emailAddress, password) {
+    const response = await this.api('/courses/' + id, 'DELETE', null, true, { emailAddress, password })
+    if(response.status !== 204) {
+      window.location.replace('/error')
+    }
+  }
+
+  // Makes an PUT request to update a course. 
+  async updateCourse(id, courseEdits, emailAddress, password) {
+    const response = await this.api('/courses/' + id, 'PUT', courseEdits, true, { emailAddress, password })
+    if (response.status === 204) {
+      return [];
+    }
+    else if (response.status === 400) {
+      return response.json().then(data => {
+        return data.errors;
+      });
+    }
+    else {
+      window.location.replace('/error')
+    }
+  }
+
+  // Makes a POST request to create a course. 
+  async createCourse(courseInfo, emailAddress, password) {
+    const response = await this.api('/courses', 'POST', courseInfo, true, { emailAddress, password })
+    if (response.status === 201) {
+      return [];
+    }
+    else if (response.status === 400) {
+      return response.json().then(data => {
+        return data.errors;
+      });
+    }
+    else {
+      window.location.replace('/error')
     }
   }
 }
