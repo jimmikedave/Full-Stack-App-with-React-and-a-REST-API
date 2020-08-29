dd9f28e32530db184e06edb7fe13fe5fca1ef79c
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-08-29 19:44:31 -0400

    Update UpdateCourse.js

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,43 +1,190 @@
-import React from 'react';
+import React, { Component, useEffect } from 'react';
+import Form from './Form';
 
-export default () => (
-    <div>
-    <div id="root">
-      <div>
-        </div>
-        <hr />
+export default class UserSignUp extends Component {
+  state = {
+    title: '',
+    description: '',
+    estimatedTime: '',
+    materialsNeeded: '',
+    userId: '',
+    errors: [],
+  }
+
+  componentDidMount() {
+    this.loadCourse();
+  }
+
+  // Collect targetted course information.
+  loadCourse = () => {
+    this.props.context.actions.courseInfo(this.props.match.params.id)
+    
+    .then(response => {
+      this.setState({
+        title: response.course.title,
+        description: response.course.description,
+        estimatedTime: response.course.estimatedTime,
+        materialsNeeded:response.course.materialsNeeded,
+        userId: response.course.userId
+      })
+    })
+    .catch(error => {
+      console.log('Error fetching and parsing data', error)
+    })
+  };
+
+  render() {
+    const {
+      title,
+      description,
+      estimatedTime,
+      materialsNeeded,
+      userId,
+      errors,
+    } = this.state;
+
+    // If the current user does not have authentication for the course redirect to /forbidden.
+    if(this.props.context.courseDetail.userId !== this.props.context.authenticatedUser.authUser.id) {
+      this.props.history.push('/forbidden')
+    }
+
+    return (
         <div className="bounds course--detail">
-          <h1>Update Course</h1>
-          <div>
-            <form>
-              <div className="grid-66">
+            <h1 className="white-text">Update Course</h1>
+            <Form 
+            cancel={this.cancel}
+            errors={errors}
+            submit={this.submit}
+            submitButtonText="Update Course"
+            elements={() => (
+            <div>
+            
+                <div className="grid-66">
                 <div className="course--header">
-                  <h4 className="course--label">Course</h4>
-                  <div><input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..." defaultValue="Build a Basic Bookcase" /></div>
-                  <p>By Joe Smith</p>
+                    <h4 className="course--label">Course</h4>
+                    <div>
+                    <input
+                        id="title"
+                        name="title"
+                        type="text"
+                        className="input-title course--title--input"
+                        placeholder="Course title..."
+                        onChange={this.change}
+                        defaultValue={title}
+                    />
+                    </div>
+                    <p>By Joe Smith</p>
                 </div>
                 <div className="course--description">
-                  <div><textarea id="description" name="description" className placeholder="Course description..." defaultValue={"High-end furniture projects are great to dream about. But unless you have a well-equipped shop and some serious woodworking experience to draw on, it can be difficult to turn the dream into a reality.\n\nNot every piece of furniture needs to be a museum showpiece, though. Often a simple design does the job just as well and the experience gained in completing it goes a long way toward making the next project even better.\n\nOur pine bookcase, for example, features simple construction and it's designed to be built with basic woodworking tools. Yet, the finished project is a worthy and useful addition to any room of the house. While it's meant to rest on the floor, you can convert the bookcase to a wall-mounted storage unit by leaving off the baseboard. You can secure the cabinet to the wall by screwing through the cabinet cleats into the wall studs.\n\nWe made the case out of materials available at most building-supply dealers and lumberyards, including 1/2 x 3/4-in. parting strip, 1 x 2, 1 x 4 and 1 x 10 common pine and 1/4-in.-thick lauan plywood. Assembly is quick and easy with glue and nails, and when you're done with construction you have the option of a painted or clear finish.\n\nAs for basic tools, you'll need a portable circular saw, hammer, block plane, combination square, tape measure, metal rule, two clamps, nail set and putty knife. Other supplies include glue, nails, sandpaper, wood filler and varnish or paint and shellac.\n\nThe specifications that follow will produce a bookcase with overall dimensions of 10 3/4 in. deep x 34 in. wide x 48 in. tall. While the depth of the case is directly tied to the 1 x 10 stock, you can vary the height, width and shelf spacing to suit your needs. Keep in mind, though, that extending the width of the cabinet may require the addition of central shelf supports."} /></div>
+                    <div>
+                    <textarea
+                        id="description"
+                        name="description"
+                        className=""
+                        placeholder="Course description..."
+                        onChange={this.change}
+                        defaultValue={description}
+                    />
+                    </div>
                 </div>
-              </div>
-              <div className="grid-25 grid-right">
+                </div>
+                <div className="grid-25 grid-right">
                 <div className="course--stats">
-                  <ul className="course--stats--list">
+                    <ul className="course--stats--list">
                     <li className="course--stats--list--item">
-                      <h4>Estimated Time</h4>
-                      <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input" placeholder="Hours" defaultValue="14 hours" /></div>
+                        <h4>Estimated Time</h4>
+                        <div>
+                        <input
+                            id="estimatedTime"
+                            name="estimatedTime"
+                            type="text"
+                            className="course--time--input"
+                            placeholder="Hours"
+                            onChange={this.change}
+                            defaultValue={estimatedTime}
+                        />
+                        </div>
                     </li>
                     <li className="course--stats--list--item">
-                      <h4>Materials Needed</h4>
-                      <div><textarea id="materialsNeeded" name="materialsNeeded" className placeholder="List materials..." defaultValue={"* 1/2 x 3/4 inch parting strip\n* 1 x 2 common pine\n* 1 x 4 common pine\n* 1 x 10 common pine\n* 1/4 inch thick lauan plywood\n* Finishing Nails\n* Sandpaper\n* Wood Glue\n* Wood Filler\n* Minwax Oil Based Polyurethane\n"} /></div>
+                        <h4>Materials Needed</h4>
+                        <div>
+                        <textarea
+                            id="materialsNeeded"
+                            name="materialsNeeded"
+                            className=""
+                            placeholder="List materials..."
+                            onChange={this.change}
+                            defaultValue={materialsNeeded}
+                        />
+                        </div>
                     </li>
-                  </ul>
+                    </ul>
                 </div>
-              </div>
-              <div className="grid-100 pad-bottom"><button className="button" type="submit">Update Course</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='course-detail.html';">Cancel</button></div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-);
+                </div>
+            </div>
+            )} />
+        </div> 
+    );
+  }
+
+  // Allows change in the input values.
+  change = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState(() => {
+      return {
+        [name]: value
+      };
+    });
+  }
+
+  // On submit update course.
+  submit = () => {
+    const { context } = this.props;
+    const authUser = context.authenticatedUser.authUser.emailAddress;
+    const authPassword = context.authPassword;
+    const courseId = context.courseDetail.id;
+
+    // console.log(this.props)
+
+    
+    // Destructure and unpack properties from the state object.
+    const {
+      title,
+      description,
+      estimatedTime,
+      materialsNeeded,
+      userId
+    } = this.state;
+
+    // New user payload that will be passed to the createUser() method
+    const courseBody = {
+      title,
+      description,
+      estimatedTime,
+      materialsNeeded,
+      userId
+    };
+
+    
+
+    // CreateUser() is an asynchronous operation that returns a promise.
+    context.data.updateCourse(courseId, courseBody, authUser, authPassword)
+    .then(errors => {
+      if(errors.length) { //handles sign up issues ex. empty array
+        this.setState({errors});
+      } else {
+        this.props.history.push('/');
+      }
+    })
+    .catch (err => {
+      console.log(err); // handle rejected promises
+      this.props.history.push('/error'); //push to history stack
+    })
+  }
+
+  cancel = () => {
+      this.props.history.push('/');
+  }
+}
