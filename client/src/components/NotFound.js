db432c9361cdf96d2a604b4b67f9419a85751302
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-08-29 19:44:28 -0400

    Update NotFound.js

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default () => (
-  <div className="bounds">
+  <div className="bounds white-text">
     <h1>Not Found</h1>
     <p>Sorry! We couldn't find the page you're looking for.</p>
+    <Link className="button button-secondary" to="/">Return to List</Link>
   </div>
 );
