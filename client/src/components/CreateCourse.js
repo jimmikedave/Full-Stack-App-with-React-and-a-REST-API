850f00a0b3eee4cab1d8cc8c12c3466ab52e318a
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-08-29 19:44:14 -0400

    Update CreateCourse.js

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -1,52 +1,157 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Form from './Form';
 
-export default () => (
-    <div>
-    <div id="root">
-      <div>
-        <hr />
+export default class UserSignUp extends Component {
+  state = {
+    title: '',
+    description: '',
+    estimatedTime: '',
+    materialsNeeded: '',
+    userId: this.props.context.authenticatedUser.authUser.id,
+    errors: [],
+  }
+
+  render() {
+    const {
+      title,
+      description,
+      estimatedTime,
+      materialsNeeded,
+      userId,
+      errors,
+    } = this.state;
+
+    return (
         <div className="bounds course--detail">
-          <h1>Create Course</h1>
-          <div>
+            <h1 className="white-text">Create Course</h1>
+            <Form 
+            cancel={this.cancel}
+            errors={errors}
+            submit={this.submit}
+            submitButtonText="Create Course"
+            elements={() => (
             <div>
-              <h2 className="validation--errors--label">Validation errors</h2>
-              <div className="validation-errors">
-                <ul>
-                  <li>Please provide a value for "Title"</li>
-                  <li>Please provide a value for "Description"</li>
-                </ul>
-              </div>
-            </div>
-            <form>
-              <div className="grid-66">
+            
+                <div className="grid-66">
                 <div className="course--header">
-                  <h4 className="course--label">Course</h4>
-                  <div><input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..." defaultValue /></div>
-                  <p>By Joe Smith</p>
+                    <h4 className="course--label">Course</h4>
+                    <div>
+                    <input
+                        id="title"
+                        name="title"
+                        type="text"
+                        className="input-title course--title--input"
+                        placeholder="Course title..."
+                        onChange={this.change}
+                    />
+                    </div>
+                    <p>By Joe Smith</p>
                 </div>
                 <div className="course--description">
-                  <div><textarea id="description" name="description" className placeholder="Course description..." defaultValue={""} /></div>
+                    <div>
+                    <textarea
+                        id="description"
+                        name="description"
+                        className=""
+                        placeholder="Course description..."
+                        onChange={this.change}
+                    />
+                    </div>
                 </div>
-              </div>
-              <div className="grid-25 grid-right">
+                </div>
+                <div className="grid-25 grid-right">
                 <div className="course--stats">
-                  <ul className="course--stats--list">
+                    <ul className="course--stats--list">
                     <li className="course--stats--list--item">
-                      <h4>Estimated Time</h4>
-                      <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input" placeholder="Hours" defaultValue /></div>
+                        <h4>Estimated Time</h4>
+                        <div>
+                        <input
+                            id="estimatedTime"
+                            name="estimatedTime"
+                            type="text"
+                            className="course--time--input"
+                            placeholder="Hours"
+                            onChange={this.change}
+                        />
+                        </div>
                     </li>
                     <li className="course--stats--list--item">
-                      <h4>Materials Needed</h4>
-                      <div><textarea id="materialsNeeded" name="materialsNeeded" className placeholder="List materials..." defaultValue={""} /></div>
+                        <h4>Materials Needed</h4>
+                        <div>
+                        <textarea
+                            id="materialsNeeded"
+                            name="materialsNeeded"
+                            className=""
+                            placeholder="List materials..."
+                            onChange={this.change}
+                        />
+                        </div>
                     </li>
-                  </ul>
+                    </ul>
                 </div>
-              </div>
-              <div className="grid-100 pad-bottom"><button className="button" type="submit">Create Course</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button></div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+                </div>
+            </div>
+            )} />
+        </div> 
+    );
+  }
+
+  // Allows change in the input values.
+  change = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    
+    this.setState(() => {
+      return {
+        [name]: value
+      };
+    });
+  }
+
+  // On submit create a course.
+  submit = () => {
+    const { context } = this.props;
+    const authUser = context.authenticatedUser.authUser.emailAddress;
+    const authPassword = context.authPassword;
+
+    console.log(context)
+
+    
+    // Destructure and unpack properties from the state object.
+    const {
+      title,
+      description,
+      estimatedTime,
+      materialsNeeded,
+      userId
+    } = this.state;
+
+    // New user payload that will be passed to the createUser() method.
+    const courseBody = {
+      title,
+      description,
+      estimatedTime,
+      materialsNeeded,
+      userId
+    };
+
+    // CreateUser() is an asynchronous operation that returns a promise.
+    context.data.createCourse(courseBody, authUser, authPassword)
+    .then(errors => {
+      console.log(errors)
+      if(errors.length) { //Handles sign up issues ex. empty array
+        this.setState({errors});
+      } else {
+        this.props.history.push('/');
+      }
+    })
+    .catch (err => {
+      console.log(err); // Handle rejected promises
+      this.props.history.push('/error'); //Push to history stack.
+    })
+  }
+
+  cancel = () => {
+      this.props.history.push('/');
+  }
+}
