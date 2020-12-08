db01d6d23e4c0ea1bf99b5e9faa2d5e457f3b4b1
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-12-08 09:46:53 -0500

    Updates

---
+++ precache-manifest.3254f93f7d4a7176cba122b560b8de36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39358efd11242966dfc2b9ef2aec6b3",
+    "url": "/Full-Stack-App-with-React-and-a-REST-API/index.html"
+  },
+  {
+    "revision": "581dd32f1cc65e51c901",
+    "url": "/Full-Stack-App-with-React-and-a-REST-API/static/css/main.d94c842b.chunk.css"
+  },
+  {
+    "revision": "c4cf8dd0d778d0249f50",
+    "url": "/Full-Stack-App-with-React-and-a-REST-API/static/js/2.56089079.chunk.js"
+  },
+  {
+    "revision": "de81bfc3d566733bec20fcc6cadc32a6",
+    "url": "/Full-Stack-App-with-React-and-a-REST-API/static/js/2.56089079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "581dd32f1cc65e51c901",
+    "url": "/Full-Stack-App-with-React-and-a-REST-API/static/js/main.afe098d3.chunk.js"
+  },
+  {
+    "revision": "8dfe746af3bce7cf6952",
+    "url": "/Full-Stack-App-with-React-and-a-REST-API/static/js/runtime-main.24593c90.js"
+  }
+]);
